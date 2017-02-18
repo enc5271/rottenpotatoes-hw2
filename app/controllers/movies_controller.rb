a86ce2ab957f283943953f1b6142dbd0dfcc0e33
enc5271 <ecochrane@tamu.edu>
2017-02-17 23:48:32 -0600

    part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
     if !@sort.nil?
       @movies = @movies.order(@sort)
     end
-    session[:sort] = @sort
+    
     #Part 2
     @all_ratings = Movie.ratings
     @ratings = params[:ratings] ? params[:ratings] : session[:ratings]
@@ -30,7 +30,9 @@ class MoviesController < ApplicationController
     else
       @ratings = Movie.ratings
     end
-    session[:ratings] = @ratings
+    #Part 3
+    #session[:sort] = @sort
+    #session[:ratings] = @ratings
   end
 
   def new
